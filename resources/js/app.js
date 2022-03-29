f5eb9faaa6c01280c32ef463011b3ce481b8e1a1
shemnduati <nduatishem@gmail.com>
2022-03-29 15:07:23 +0300

    strutured the tables and the main dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+let routes = [
+    
+    { path: '/order', component: require('./components/Dashboard.vue').default },
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
